98c06fd925fc055986a948626682cfc9c962b5d1
Geneviève Plante-Brisebois <gen450@gmail.com>
2019-03-03 01:32:38 -0500

    finished adjusting the basic functionnalities

--- file_server.py
+++ file_server.py
@@ -6,13 +6,20 @@ import os
 import server_lib
 
 
+# gets the list of the files in the directory
 def get_file_list():
     list_f = []
     for (dirpath, dirnames, filenames) in os.walk('.'):
         list_f.extend(filenames)
-    return list_f
+    final_msg = ""
+    for x in list_f:
+        final_msg += x
+        final_msg += ", \n"
+
+    return final_msg
 
 
+# gets the content of the foo file
 def get_foo(sck):
     filename = "foo.txt"
     # verification if the file exists
@@ -42,6 +49,7 @@ def post_bar(content, conn):
     if os.path.isfile(filename):
         file = open(filename, "w")
         file.write(content)
+        file.close()
     # if there is no file called bar
     else:
         message = 'file not existent, creating file and write to file. \n'
@@ -49,6 +57,7 @@ def post_bar(content, conn):
         conn.send(message.encode('utf-8'))
         file = open("bar.txt", "w+")
         file.write(content)
+        file.close()
 
 
 def main():
@@ -58,23 +67,27 @@ def main():
     while True:
         client_con, client_add = sck.accept()
         req = server_lib.response_to_client(client_con, client_add)
-
+        print("\n Request received, handling request \n")
         # checking the first char of the requests being made in order to react the proper way
         if req.decode('utf-8')[:7] == "GET/foo":
-            msg = "getting list of files \n"
-            client_con.send(msg.encode('utf-8'))
             get_foo(client_con)
         elif req.decode('utf-8')[:4] == "GET/":
+            msg = "\n getting list of files \n"
+            client_con.send(msg.encode('utf-8'))
             list_files = get_file_list()
             print(list_files)
-            client_con.send(list_files)
+            # final_list = [item.encode('utf-8') for item in list_files]
+            # final_list = json.dumps(list_files)
+            client_con.send(list_files.encode('utf-8'))
         elif req.decode('utf-8')[:8] == "POST/bar":
+            msg = ""
             # send the string without the POST/bar (so just the content of the file)
             post_bar(req.decode('utf-8')[9:], client_con)
-
-        elif req.decode('utf-8') == "CLOSE":
-            client_con.close()
-
+        else:
+            err = "\n Request invalid, enter a valid request \n"
+            client_con.send(err.encode('utf-8'))
+        # closing client connection
+        client_con.close()
 
 # operate the main function and bring the server online
 main()
